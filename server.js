284a6ef4bc20738a6040fb393169aaf0e85f2429
Iyunus1 <90559412+Iyunus1@users.noreply.github.com>
2024-12-07 02:06:43 +0000

    skeleton for DELETE and POST has been created, POST request filtering for number or name not existing sends and error and the same name is also given an error

--- server.js
+++ server.js
@@ -43,10 +43,39 @@ app.get('/api/notes/:id', (req, res) => {
 
 })
 
+const generateId = () => {
+  const maxId = phoneBook.length > 0 ? Math.max(...phoneBook.map((person) => Number(person.id))) : 0;
+  return String(maxId + 1)
+}
+
 app.post('/api/notes', (req, res) => {
   const entry = req.body;
-  console.log(entry)
-  res.json(entry)
+
+
+ 
+   if(!entry.number){
+    return res.status(400).json({
+      error: 'number missing'
+    })
+  } else if(!entry.name ){
+    return res.status(400).json({
+      error: 'name missing'
+    })
+  } else if(phoneBook.find((person) => entry.name.toLowerCase() === person.name.toLowerCase())){
+    return res.status(400).json({
+      error: 'unique name required'
+    })
+  }
+
+  const note = {
+    id: generateId(),
+    name: entry.name,
+    number: entry.number
+  }
+
+  phoneBook.push(note)
+  console.log(note)
+  res.json(note)
 })
 
 app.delete('/api/notes/:id', (req, res) => {
